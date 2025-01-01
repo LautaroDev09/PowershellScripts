5974a5b7be191c2ee737978189b42c7a8fe42d42
Lautaro Rodriguez <lautarorod10@gmail.com>
2025-01-01 16:56:16 -0300

    Create TaskbarFix.ps1
    Description:
    This PowerShell script resolves an issue with the Windows 10 taskbar caused after migrating a device from a local domain to JumpCloud's cloud-based solution. It identifies and removes specific registry keys that may cause conflicts, restoring normal taskbar functionality.
    
    Usage:
    Run the script as the affected user to ensure the registry keys in the user's context are correctly addressed.

---
+++ TaskbarFix.ps1
@@ -0,0 +1,18 @@
+﻿# Registry keys to check and delete
+$registryKeys = @(
+    "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\StartLayout",
+    "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Start",
+    "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\SearchSettings",
+    "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Search",
+    "HKCU:SOFTWARE\Microsoft\Active Setup\Installed Components\ADMU-AppxPackage"
+)
+
+# Check and delete registry keys
+foreach ($key in $registryKeys) {
+    if (Test-Path $key) {
+        Write-Host "Deleting registry key: $key"
+        Remove-Item -Path $key -Force -Recurse
+    } else {
+        Write-Host "Registry key does not exist: $key"
+    }
+}
